1b09ec648c72c24936e6dd4f50720343dc094d2f
TLGG16 <TLGG16@yandex.ru>
2023-06-14 21:34:39 +0300

    kekw

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,32 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        List<String> fileData = new ArrayList<>();
+        try (FileInputStream fileStream = new FileInputStream(file)) {
+            fileData = Arrays.asList( new String(fileStream.readAllBytes()).split("\n"));
+        } catch (IOException e) {
+            return profile;
+        }
+        try {
+            profile.setName(fileData.get(0).split("[ :]")[2].trim());
+            profile.setAge(Integer.parseInt(fileData.get(1).split("[ :]")[2].trim()));
+            profile.setEmail(fileData.get(2).split("[ :]")[2].trim());
+            profile.setPhone(Long.parseLong(fileData.get(3).split("[ :]")[2].trim()));
+        }
+        catch (NumberFormatException e){
+            return profile;
+        }
+        return profile;
     }
 }
